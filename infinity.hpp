bb802692685daa3de681bd53596683492965b2dd
Python_Works <andrewdemiranda@gmail.com>
2017-10-31 10:39:26 -0300

    Create infinity.hpp

---
+++ infinity.hpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+
+
+using namespace std;
+
+extern string version_lib;
+version_lib = "01.01-infinity-0001"
+
+int demos_of_lang() {
+    system("git clone https://github.com/Pyworks/Infinity-Lang-Demo.git ");
+}
